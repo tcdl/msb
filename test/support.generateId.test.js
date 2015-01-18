ab8363d6fcc679467cfab552b6418547685fe109
jupiter <pieter@wavana.com>
2015-01-18 12:08:50 +0000

    Added tests for unique id generator

---
+++ test/support.generateId.test.js
@@ -0,0 +1,75 @@
+/* Setup */
+var Lab = require('lab');
+var Code = require('code');
+var lab = exports.lab = Lab.script();
+
+var describe = lab.describe;
+var it = lab.it;
+var before = lab.before;
+var beforeEach = lab.beforeEach;
+var after = lab.after;
+var afterEach = lab.afterEach;
+var expect = Code.expect;
+
+/* Modules */
+var _ = require('lodash');
+var simple = require('simple-mock');
+var generateId = require('../lib/support/generateId');
+
+describe('generateId', function() {
+  afterEach(function(done) {
+    simple.restore();
+    done();
+  });
+
+  it('generates ordered ids', function(done) {
+    var ids = [];
+
+    while (ids.length < 10000) {
+      ids.push(generateId());
+    }
+
+    expect(ids.length).equals(10000);
+    expect(ids.join()).equals(ids.sort().join());
+    done();
+  });
+
+  it('works with constant date', function(done) {
+    var constantDate = Date.now() + 1000;
+    var ids = [];
+
+    simple.mock(Date, 'now', function() {
+      return constantDate;
+    });
+
+    while (ids.length < 65536) {
+      ids.push(generateId());
+    }
+
+    expect(ids.length).equals(65536);
+    expect(ids.length).equals(_.uniq(ids).length);
+    done();
+  });
+
+  it('works with failure of randomness', function(done) {
+    var constantDate = Date.now() + 1000;
+
+    simple.mock(Date, 'now', function() {
+      return constantDate;
+    });
+
+    simple.mock(Math, 'random', function() {
+      return 0;
+    });
+
+    var ids = [];
+
+    while (ids.length < 65536) {
+      ids.push(generateId());
+    }
+
+    expect(ids.length).equals(65536);
+    expect(ids.length).equals(_.uniq(ids).length);
+    done();
+  });
+});
