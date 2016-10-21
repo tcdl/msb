cc4d42a335a0acbc2d15c07001d450e2779b24ce
sergiv83 <sergejivanoff@gmail.com>
2016-10-21 11:26:58 +0300

    error in test

--- test/serviceDetails.test.js
+++ test/serviceDetails.test.js
@@ -1,84 +1,89 @@
-/* Setup */
-var expect = require('chai').expect;
-/* Modules */
-var _ = require('lodash');
-var simple = require('simple-mock');
-var path = require('path');
-var serviceDetailsModulePath = path.resolve(__dirname, '../lib/support/serviceDetails.js');
-describe.only('serviceDetails', function () {
+"use strict";
+var chai_1 = require("chai");
+var path_1 = require("path");
+var serviceDetailsModulePath = path_1.resolve(__dirname, "../lib/support/serviceDetails.js");
+var simple = require("simple-mock");
+describe.only("serviceDetails", function () {
     var serviceDetails;
     beforeEach(function (done) {
         delete (require.cache[serviceDetailsModulePath]);
         done();
     });
-    it('should set serviceDetails dynamically', function (done) {
-        simple.mock(require('os'), 'hostname').returnWith('abchost');
+    it.only("should set serviceDetails dynamically", function (done) {
+        var fakeInterfaces = { en0: [{ address: "1.2.3.4",
+                    netmask: "255.255.255.0",
+                    family: "IPv4",
+                    mac: "60:03:08:92:27:88",
+                    internal: false }] };
+        simple.mock(require("os"), "networkInterfaces").returnWith(fakeInterfaces);
+        simple.mock(require("pkginfo"), "name").returnWith("");
+        serviceDetails = require(serviceDetailsModulePath);
+        // expect(serviceDetails.hostname).equals("abchost");
+        chai_1.expect(serviceDetails.ip).equals("1.2.3.4");
+        chai_1.expect(serviceDetails.pid).equals(process.pid);
+        chai_1.expect(serviceDetails.name).to.be.a("string");
+        chai_1.expect(!!serviceDetails.version.match(/\d+\.\d+\.\d+/)).to.be.true;
+        chai_1.expect(serviceDetails.instanceId).length(24);
+        done();
+    });
+    it.only("should set host 'unknown' on configured incorrectly host", function (done) {
+        simple.mock(require("os"), "hostname").throwWith(new Error());
+        serviceDetails = require(serviceDetailsModulePath);
+        chai_1.expect(serviceDetails.hostname).equals("unknown");
+        chai_1.expect(serviceDetails.pid).equals(process.pid);
+        chai_1.expect(serviceDetails.name).to.be.a("string");
+        chai_1.expect(!!serviceDetails.version.match(/\d+\.\d+\.\d+/)).to.be.true;
+        chai_1.expect(serviceDetails.instanceId).length(24);
+        done();
+    });
+    //TODO: fix this test and all tests below after Nikita finds the solution for nested objects
+    it.only("should safely handle a lack of mainModule", function (done) {
+        simple.mock(process, "mainModule", undefined);
         serviceDetails = require(serviceDetailsModulePath);
         console.log(serviceDetails);
-        process.exit();
-        expect(serviceDetails.hostname).equals('abchost');
-        expect(serviceDetails.ip).equals('1.2.3.4');
-        expect(serviceDetails.pid).equals(process.pid);
-        expect(serviceDetails.name).to.be.a('string');
-        expect(!!serviceDetails.version.match(/\d+\.\d+\.\d+/)).to.be.true;
-        expect(serviceDetails.instanceId).length(24);
+        chai_1.expect(serviceDetails.name).equals(undefined);
+        chai_1.expect(serviceDetails.version).equals(undefined);
+        chai_1.expect(serviceDetails.instanceId).length(24);
         done();
     });
-    it('should set host "unknown" on configured incorrectly host', function (done) {
-        simple.mock(require('os'), 'hostname').throwWith(new Error());
+    it("should safely handle a missing package.json", function (done) {
+        simple.mock(process, "mainModule", { paths: ["/tmp/etc.js"] });
         serviceDetails = require(serviceDetailsModulePath);
-        expect(serviceDetails.hostname).equals('unknown');
-        expect(serviceDetails.pid).equals(process.pid);
-        expect(serviceDetails.name).to.be.a('string');
-        expect(!!serviceDetails.version.match(/\d+\.\d+\.\d+/)).to.be.true;
-        expect(serviceDetails.instanceId).length(24);
+        chai_1.expect(serviceDetails.name).equals(undefined);
+        chai_1.expect(serviceDetails.version).equals(undefined);
+        chai_1.expect(serviceDetails.instanceId).length(24);
         done();
     });
-    it('should safely handle a lack of mainModule', function (done) {
-        simple.mock(process, 'mainModule', undefined);
+    it("should handle a valid package.json", function (done) {
+        simple.mock(process, "mainModule", { paths: [require("path").join(__dirname, "fixtures", "package.json")] });
         serviceDetails = require(serviceDetailsModulePath);
-        expect(serviceDetails.name).equals(undefined);
-        expect(serviceDetails.version).equals(undefined);
-        expect(serviceDetails.instanceId).length(24);
+        console.log(serviceDetails);
+        chai_1.expect(serviceDetails.name).equals("example");
+        chai_1.expect(serviceDetails.version).equals("1.0.0");
+        chai_1.expect(serviceDetails.instanceId).length(24);
         done();
     });
-    it('should safely handle a missing package.json', function (done) {
-        simple.mock(process, 'mainModule', { paths: ['/tmp/etc.js'] });
-        serviceDetails = require(serviceDetailsModulePath);
-        expect(serviceDetails.name).equals(undefined);
-        expect(serviceDetails.version).equals(undefined);
-        expect(serviceDetails.instanceId).length(24);
-        done();
-    });
-    it('should handle a valid package.json', function (done) {
-        simple.mock(process, 'mainModule', { paths: [require('path').join(__dirname, 'fixtures', 'package.json')] });
-        serviceDetails = require(serviceDetailsModulePath);
-        expect(serviceDetails.name).equals('example');
-        expect(serviceDetails.version).equals('1.0.0');
-        expect(serviceDetails.instanceId).length(24);
-        done();
-    });
-    it('should handle a valid package.json without version and name fields', function (done) {
-        var path = require('path').join(__dirname, 'fixtures', 'package.json');
+    it("should handle a valid package.json without version and name fields", function (done) {
+        var path = require("path").join(__dirname, "fixtures", "package.json");
         var pkg = require(path);
-        simple.mock(pkg, 'name', undefined);
-        simple.mock(pkg, 'version', undefined);
-        simple.mock(process, 'mainModule', { paths: [path] });
+        simple.mock(pkg, "name", undefined);
+        simple.mock(pkg, "version", undefined);
+        simple.mock(process, "mainModule", { paths: [path] });
         serviceDetails = require(serviceDetailsModulePath);
-        expect(serviceDetails.name).equals(undefined);
-        expect(serviceDetails.version).equals(undefined);
-        expect(serviceDetails.instanceId).length(24);
+        chai_1.expect(serviceDetails.name).equals(undefined);
+        chai_1.expect(serviceDetails.version).equals(undefined);
+        chai_1.expect(serviceDetails.instanceId).length(24);
         done();
     });
-    it('should set some of serviceDetails by environment variables', function (done) {
-        simple.mock(process, 'mainModule', { paths: [require('path').join(__dirname, 'fixtures', 'package.json')] });
-        simple.mock(process.env, 'MSB_SERVICE_NAME', 'special-name');
-        simple.mock(process.env, 'MSB_SERVICE_VERSION', '999');
-        simple.mock(process.env, 'MSB_SERVICE_INSTANCE_ID', 'abc123');
+    it("should set some of serviceDetails by environment variables", function (done) {
+        simple.mock(process, "mainModule", { paths: [require("path").join(__dirname, "fixtures", "package.json")] });
+        simple.mock(process.env, "MSB_SERVICE_NAME", "special-name");
+        simple.mock(process.env, "MSB_SERVICE_VERSION", "999");
+        simple.mock(process.env, "MSB_SERVICE_INSTANCE_ID", "abc123");
         serviceDetails = require(serviceDetailsModulePath);
-        expect(serviceDetails.name).equals('special-name');
-        expect(serviceDetails.version).equals('999');
-        expect(serviceDetails.instanceId).equals('abc123');
+        chai_1.expect(serviceDetails.name).equals("special-name");
+        chai_1.expect(serviceDetails.version).equals("999");
+        chai_1.expect(serviceDetails.instanceId).equals("abc123");
         done();
     });
 });
