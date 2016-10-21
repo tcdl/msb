7d59ebd135bbf55a7c44062341fed36d6e971625
sergiv83 <sergejivanoff@gmail.com>
2016-10-21 11:25:22 +0300

    change in linter : space at the begin of comment is not mandatory any more one test fixed with TS syntax fix in serviceDetails (missing ;)

---
+++ test/support.logger.test.ts
@@ -0,0 +1,24 @@
+import {expect} from "chai";
+import {warn} from "../lib/support/logger";
+const simple = require("simple-mock");
+
+describe("logger", function() {
+
+  afterEach(function(done) {
+    simple.restore();
+    done();
+  });
+
+  describe("warn()", function() {
+    it("should log error to console", function(done) {
+      simple.mock(console, "error").returnWith();
+
+      warn("abc");
+
+      expect((console.error as any).calls).length(1);
+      expect((console.error as any).lastCall.arg).equals("WARNING: abc");
+
+      done();
+    });
+  });
+});
