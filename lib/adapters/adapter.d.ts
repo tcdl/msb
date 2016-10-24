7a7d00bd2394fe58a3b656a296f7dc8f88d46995
Anton Shumsky <anton.shumsky@gmail.com>
2016-10-24 12:55:44 +0300

    Create definitions of adapter interfaces; use interfaces in AMQP adapter.

---
+++ lib/adapters/adapter.d.ts
@@ -0,0 +1,25 @@
+import {Message} from "../messageFactory";
+import {EventEmitter} from "events";
+import {BrokerConfig} from "../config";
+
+interface BrokerAdapter {
+  Publish(config: BrokerConfig): BrokerAdapterPublisherFactory;
+  Subscribe(config: BrokerConfig): BrokerAdapterSubscriber;
+  close(): void;
+}
+
+interface BrokerAdapterPublisherFactory {
+  channel(topic: string): BrokerAdapterPublisher;
+}
+
+interface BrokerAdapterPublisher {
+  publish(message: Message, cb: (err?: Error) => void): void;
+  close(): void;
+}
+
+interface BrokerAdapterSubscriber extends EventEmitter {
+  onceConsuming(cb: Function): void;
+  confirmProcessedMessage(message: Message, _safe: boolean): void;
+  rejectMessage(message: Message): void;
+  close(): void;
+}
