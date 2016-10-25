8f712a77ecd68b47f0de566352d7193fc8fa244e
sergiv83 <sergejivanoff@gmail.com>
2016-10-25 18:06:55 +0300

    draft

--- test/examples/responder.ts
+++ test/examples/responder.ts
@@ -1,42 +1,39 @@
-// const msb = require("../..");
-// const Responder = msb.Responder;
-// const validateWithSchema = msb.validateWithSchema;
-// let i = 1001;
-// const payloadSchema = {
-//  type: "object",
-//  properties: {
-//    body: { type: "object" }
-//  }
-// };
-//
-// module.exports = Responder.createServer({
-//  namespace: "test:general",
-//  tags: ["b"]
-// })
-// // Validation middleware
-// .use(validateWithSchema.middleware(payloadSchema))
-// // Arrays of middleware
-// .use([
-//  function(request, response, next) {
-//    if (request.body && request.body.instruction === "error") return next(new Error());
-//    next();
-//  },
-//  function(request, response, next) {
-//    response.responder.sendAck(1500, next); // Requester should wait at least 1.5s for response
-//  }
-// ])
-// // Normal response
-// .use(function(request, response, next) {
-//  response.body = i++;
-//  response.end();
-// })
-// // Custom error handler
-// .use(function(err, request, response, next) {
+import {Responder, validateWithSchema, msb} from "../../msb";
+
+let i = 1001;
+const payloadSchema: msb.JsonSchema = {
+ type: "object",
+ properties: {
+   body: { type: "object" }
+ }
+};
+
+module.exports = Responder.createServer({
+ namespace: "test:general"
+ // tags: ["b"]
+})
+// Validation middleware
+.use(validateWithSchema.middleware(payloadSchema))
+// Arrays of middleware
+.use(function(request, response, next) {
+   if ((request as any).body && (request as any).body.instruction === "error") return next(new Error());
+   next();
+})
+.use(function(request, response, next) {
+    (response as any).responder.sendAck(1500, next); // Requester should wait at least 1.5s for response
+})
+// Normal response
+.use(function(request, response, next) {
+    (response as any).body = i++;
+    (response as any).end();
+});
+// Custom error handler
+// .use(function(err: Error, request: Object, response: Object, next: Function): any {
 //  if (err.name === "SchemaValidationError") return next();
 //
 //  response.writeHead(500);
 //  response.body = "Special Message";
 //  response.end();
 // });
-//
-// if (!module.parent) module.exports.listen();
+
+if (!module.parent) module.exports.listen();
