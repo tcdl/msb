47fe3fe1a6aa668dc64f0b54af39390f208d9bdd
Andrii Grytsyk <a.gritsik@gmail.com>
2017-01-17 17:21:41 +0200

    finilize publisher test

--- test/unit/api/publisher.spec.ts
+++ test/unit/api/publisher.spec.ts
@@ -18,7 +18,7 @@ describe("api.Publisher", () => {
     sandbox.restore();
   });
 
-  it("should publish message with defaults only if default configuration is used", () => {
+  it("should publish message with defaults only if configuration is not provided", () => {
 
     // arrange and assert
     sandbox.stub(channelManager, "findOrCreateProducer", (topic, brokerConfig) => {
@@ -30,15 +30,13 @@ describe("api.Publisher", () => {
         publish: (message) => {
           // assert message meta
           expect(message.meta.ttl).to.be.null;
-          expect(message.meta.publishedAt).to.not.be.null;
-          expect(message.meta.durationMs).to.not.be.null;
+          expect(message.meta.publishedAt).to.exist;
+          expect(message.meta.durationMs).to.exist;
 
-          // assert message topic, tags
+          // assert message topic, tags, payload
           expect(message.topics.to).to.equal(TOPIC);
           expect(message.topics.routingKey).to.be.undefined;
           expect(message.tags).to.eql([]);
-
-          // assert payload
           expect(message.payload).to.eql(PAYLOAD);
         },
       };
@@ -51,51 +49,27 @@ describe("api.Publisher", () => {
 
   it("should publish message including configuration if it was provided", () => {
 
-    // arrange
-    let spyPublish = sandbox.spy();
+    // arrange and assert
+    sandbox.stub(channelManager, "findOrCreateProducer", (topic, brokerConfig) => {
+      // assert channelManager properties
+      expect(topic).to.equal(TOPIC);
+      expect(brokerConfig).to.eql({type: "topic"});
 
-    const stubFindOrCreateProducer = sandbox.stub(channelManager, "findOrCreateProducer");
-    stubFindOrCreateProducer
-      .withArgs(TOPIC, {type: "topic"}, null)
-      .returns({publish: spyPublish});
+      return {
+        publish: (message) => {
+          // assert message meta
+          expect(message.meta.ttl).to.equal(TTL);
+          expect(message.meta.publishedAt).to.exist;
+          expect(message.meta.durationMs).to.exist;
 
-    // act
-    new Builder(TOPIC)
-      .withTtl(TTL)
-      .withExchangeType("topic")
-      .withRoutingKey("event:updated")
-      .withTags(["tag1", "tag2"])
-      .publish(PAYLOAD);
-
-    // assert
-    const spyMessage = spyPublish.getCall(0).args[0];
-
-    // message meta
-    expect(spyMessage.meta.ttl).to.equal(TTL);
-    expect(spyMessage.meta.publishedAt).to.not.be.null;
-    expect(spyMessage.meta.durationMs).to.not.be.null;
-
-    // message topic, tags, payload
-    expect(spyMessage.topics.to).to.equal(TOPIC);
-    expect(spyMessage.topics.routingKey).to.equal("event:updated");
-    expect(spyMessage.tags).to.eql(["tag1", "tag2"]);
-    expect(spyMessage.payload).to.eql(PAYLOAD);
-
-  });
-
-  it("11 should publish message including configuration if it was provided", () => {
-
-    // arrange
-    const mockedFindOrCreateProducer = sandbox.mock(channelManager).expects("findOrCreateProducer")
-      .once()
-      .withArgs("test:topic", {type: "topic"}, null);
-
-    const mockedPublish = sinon.expectation.create("publish")
-      .withArgs(sinon.match({meta: {ttl: 10000})
-          .and(sinon.match({tags: ["tag1", "tag2"]}))
-          .and(sinon.match({topics: {routingKey: "event:updated", to: "test:topic"}})));
-
-    mockedFindOrCreateProducer.returns({publish: mockedPublish});
+          // assert message topic, tags, payload
+          expect(message.topics.to).to.equal(TOPIC);
+          expect(message.topics.routingKey).to.equal("event:updated");
+          expect(message.tags).to.eql(["tag1", "tag2"]);
+          expect(message.payload).to.eql(PAYLOAD);
+        },
+      };
+    });
 
     // act
     new Builder(TOPIC)
@@ -104,12 +78,6 @@ describe("api.Publisher", () => {
       .withRoutingKey("event:updated")
       .withTags(["tag1", "tag2"])
       .publish(PAYLOAD);
-
-    mockedFindOrCreateProducer.once();
-    mockedFindOrCreateProducer.verify();
-
-    mockedPublish.once();
-    mockedPublish.verify();
-
   });
+
 });
