01e5e7976091b20fc069bf1b17fd4e41b3ec99f1
Andrii Grytsyk <a.gritsik@gmail.com>
2017-01-23 12:15:40 +0200

    fix import

--- test/unit/api/utils.spec.ts
+++ test/unit/api/utils.spec.ts
@@ -1,19 +1,18 @@
 import {expect} from "chai";
-import * as sinon from "sinon";
-import * as utils from "./../../../lib/api/utils";
-import {Builder as SubscriberBuilder} from "../../../lib/api/subscriber";
 import {Builder as PublisherBuilder} from "../../../lib/api/publisher";
+import {Builder as SubscriberBuilder} from "../../../lib/api/subscriber";
+import * as utils from "./../../../lib/api/utils";
 
 describe("api.utils", () => {
 
-  it("should return publisher builder", () => {
+  it("should return a publisher builder", () => {
     // act and arrange
     const builder = utils.publisher("test:topic");
     // assert
     expect(builder).to.be.instanceOf(PublisherBuilder);
   });
 
-  it("should return subscriber builder", () => {
+  it("should return a subscriber builder", () => {
     // act and arrange
     const builder = utils.subscriber("test:topic");
     // assert
