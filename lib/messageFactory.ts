0b0ecb13a9fad1447dadacfc295dbb3948ada39d
Anton Shumsky <anton.shumsky@gmail.com>
2017-02-07 18:32:34 +0200

    IN-1518. Remove message context.

--- lib/messageFactory.ts
+++ lib/messageFactory.ts
@@ -1,15 +1,9 @@
 import serviceDetails = require("./support/serviceDetails");
 import generateId = require("./support/generateId");
 
-const _ = require("lodash");
-
 const INSTANCE_ID = serviceDetails.instanceId;
 
-let contextMessage: Message = null;
-
-export function createMessage(namespace: string, payload: any, config?: MessageConfig, originalMessage?: Message): Message {
-  if (originalMessage === undefined) originalMessage = contextMessage;
-
+export function createMessage(namespace: string, payload: any, config?: MessageConfig): Message {
   const metadata: MessageMeta = {
     ttl: (config && config.ttl) || null,
     createdAt: new Date(),
@@ -19,22 +13,22 @@ export function createMessage(namespace: string, payload: any, config?: MessageC
   return {
     id: generateId(),
     correlationId: generateId(),
-    tags: _.union(config && config.tags, originalMessage && originalMessage.tags),
+    tags: config.tags,
     topics: config.routingKey ? {to: namespace, routingKey: config.routingKey} : {to: namespace},
     meta: metadata,
     payload: payload,
   };
 }
 
-export function createRequestMessage(namespace: string, payload: any, config?: MessageConfig, originalMessage?: Message): Message {
-  const message = createMessage(namespace, payload, config, originalMessage);
+export function createRequestMessage(namespace: string, payload: any, config?: MessageConfig): Message {
+  const message = createMessage(namespace, payload, config);
   message.topics.response = namespace + ":response:" + INSTANCE_ID;
 
   return message;
 }
 
 export function createResponseMessage(originalMessage: Message, payload: MessagePayload, ack: MessageAck, config?: MessageConfig): Message {
-  const message = createMessage(originalMessage.topics.response, payload, config, originalMessage);
+  const message = createMessage(originalMessage.topics.response, payload, config);
   message.correlationId = originalMessage.correlationId;
   message.ack = ack;
 
@@ -43,14 +37,6 @@ export function createResponseMessage(originalMessage: Message, payload: Message
 
 export function createAckMessage(originalMessage: Message, ack: MessageAck, config?: MessageConfig): Message {
   return createResponseMessage(originalMessage, null, ack, config);
-}
-
-export function startContext(message?: Message): void {
-  contextMessage = message;
-}
-
-export function endContext(): void {
-  contextMessage = null;
 }
 
 export interface MessageConfig {
