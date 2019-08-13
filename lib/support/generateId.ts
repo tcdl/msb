f039a427cde088e8ed63803c9192fea842a66b94
Björn Dahlgren <bjorn@dahlgren.at>
2019-08-13 12:42:09 +0200

    Fix crash when process pid is larger than 65535
    Node 10 removes noAssert from buffer.write functions

--- lib/support/generateId.ts
+++ lib/support/generateId.ts
@@ -6,10 +6,12 @@ let buf = new Buffer(12);
 buf[4] = INSTANCE_ID[0];
 buf[5] = INSTANCE_ID[1];
 
-buf.writeUInt16BE(process.pid, 6, true);
+// tslint:disable-next-line:no-bitwise
+buf.writeUInt16BE(process.pid & 0xFFFF, 6);
 
 function generateId(): string {
-  buf.writeUInt32BE(Math.floor(Date.now() / 1000) - BASE_UNIX_DATE, 0, true);
+  // tslint:disable-next-line:no-bitwise
+  buf.writeUInt32BE((Math.floor(Date.now() / 1000) - BASE_UNIX_DATE) & 0xFFFFFFFF, 0);
   buf.writeUInt16BE(inc(), 8);
   buf.writeUInt8(Math.floor(Math.random() * 256), 10);
   buf.writeUInt8(Math.floor(Math.random() * 256), 11);
